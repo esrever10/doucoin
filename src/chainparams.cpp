64ee34217ef6f3cc80b2cbf4aef74a814c44ce94
caochen <caochen@cloudist.cc>
2017-06-23 08:18:46 +0800

    reset bnProofOfWorkLimit

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -117,7 +117,7 @@ public:
         pchMessageStart[3] = 0x33;
         vAlertPubKey = ParseHex("04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
         nDefaultPort = 23333;
-        bnProofOfWorkLimit = ~uint256(0) >> 1;
+        bnProofOfWorkLimit = ~uint256(0) >> 32;
         nSubsidyHalvingInterval = 210000;
         nEnforceBlockUpgradeMajority = 750;
         nRejectBlockOutdatedMajority = 950;
